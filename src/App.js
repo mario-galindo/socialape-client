544ec51f4d1549679f4caf8e0de9893dd565ee08
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-04 20:13:38 -0600

    request to get screams

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 //Components
 import Navbar from './components/Navbar';
@@ -10,8 +12,28 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+});
+
+
 function App() {
   return (
+    <MuiThemeProvider theme={theme}>
     <div className="App">
       <Router>
       <Navbar></Navbar>
@@ -24,6 +46,7 @@ function App() {
         </div>
       </Router>
     </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 
 class home extends Component {
+
+    state = {
+        screams: null
+    }
+
+    componentDidMount() {
+        axios.get('/screams')
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    screams: res.data
+                })
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
     render() {
+
+        let recentScreamMarkup = this.state.screams ? (
+        this.state.screams.map(scream => <p>{scream.body}</p>)
+        ) : <p>Loading..</p>
+
         return (
-            <div>
-                <h1>Home Page</h1>                
-            </div>
+            <Grid container>
+                <Grid item sm={8} xs={12}>
+                    {recentScreamMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile..</p>
+                </Grid>
+            </Grid>
         )
     }
 }
