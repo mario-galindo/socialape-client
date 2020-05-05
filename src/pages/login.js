4380995671c1755a968f75e2ebd0dfd577ceaf07
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-05 14:50:48 -0600

    add scream with card material

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
+import PropTypes from 'prop-types'
+
+//MUI Stuff
+import Grid from '@material-ui/core/Grid'
+
+
+const styles = {
+    form: {
+        textAlign: 'center'
+    }
+}
 
 class login extends Component {
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <h1>Login</h1>
-            </div>
+            <Grid container className={classes.form}>
+                <Grid item sm></Grid>
+                <Grid item sm>
+                    <p>yoo</p>
+                </Grid>
+                <Grid item sm></Grid>
+            </Grid>
         )
     }
 }
 
-export default login
+login.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(login)
