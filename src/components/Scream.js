13d68ef81d1f2951565ed804b9b26b69e861ee03
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-05 09:38:08 -0600

    add format to date scream before add state auth

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 
 //MUI Stuff
@@ -26,6 +28,7 @@ const style = {
 
 class Scream extends Component {
     render() {
+        dayjs.extend(relativeTime)
         const { classes, scream: { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount } } = this.props
         return (
             <Card className={classes.card}>
@@ -34,7 +37,7 @@ class Scream extends Component {
                 title="Profile Image" className={classes.image}/>
                 <CardContent className={classes.content}>
                     <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
-                    <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+                    <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                     <Typography variant="body1">{body}</Typography>
                 </CardContent>
             </Card>
